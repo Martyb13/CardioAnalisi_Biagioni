ffe28286a830f2e6cbdaa41d9a63f3de5e18c67d
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-04-11 09:12:50 +0200

    Ho inserito uil metodo per calcolare la frequenza massima ed il numero massimo e minimo dei battiti

---
+++ CardioAnalisi_Biagioni/CardioAnalisiLibrary/DataCardio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardioAnalisiLibrary
+{
+    public class DataCardio
+    {
+        static public int Frequenza(int età, ref int min)
+        {
+            int frequenzamax = 220 - età;
+            min = frequenzamax * 70 / 100;
+            int max = frequenzamax * 90 / 100;
+            return max;
+        }
+    }
+}
---
+++ CardioAnalisi_Biagioni/DataCardio.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DataCardio.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DataCardio.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("91e999fc-ccfa-4901-9421-c4265151f29c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
